e23ae061978c2f35b9f560b1d3c82e6bf28899cd
Yordy_AV <124385874+Yoyialx@users.noreply.github.com>
2026-01-12 23:40:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function calculateBMI(){
+    const weight = parseFloat(document.getElementById("weight").value)
+    const height = parseFloat(document.getElementById("height").value) / 100
+
+    if(weight && height){
+        const bmi = (weight / (height * height)).toFixed(2)
+        document.getElementById("result").textContent = `Tu BMI es: ${bmi}`
+    }else {
+        document.getElementById("result").style.color = 'red'
+        document.getElementById("result").textContent = "Porfavor introduce valores validos."
+    }
+}
